aa88aa43687f321ab6c406d956b361dc6d96c528
Ruslan Onishchuk <ruslan.onishchuk.90@gmail.com>
2022-09-24 19:58:59 +0300

    not working attempt

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,20 +2,41 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.10"
-    id("de.undercouch.download") version "5.2.0"
+//    id("de.undercouch.download") version "5.2.0"
 }
 
 group = "com.gmail.rewheeldev"
-version = "0.0.1"
+version = "0.0.2"
 
 repositories {
     mavenCentral()
+    ivy {
+        url = uri("https://github.com/")
+        patternLayout {
+            artifact ("[organization]/[module]/raw/main/[module]-[revision].jar")
+            setM2compatible(false)
+        }
+    }
 }
 
 dependencies {
     testImplementation(kotlin("test"))
-    //
-    implementation(fileTree(mapOf("dir" to buildDir, "include" to listOf("*.jar", "*.aar"))))
+
+
+    implementation ("rewheeldev:common-utils:0.0.1"){
+
+    }
+
+//    implementation(fileTree(mapOf("dir" to buildDir, "include" to listOf("*.jar", "*.aar"))))
+//    implementation ("common-utils:0.0.1") {
+//        artifact {
+//            name = "common-utils"
+//            extension = "jar"
+//            type = "jar"
+//            url = "https://github.com/rewheeldev/common-utils/raw/main/common-utils-0.0.1.jar"
+//        }
+//    }
+
 }
 
 tasks.test {
@@ -27,19 +48,17 @@ tasks.withType<KotlinCompile> {
 }
 
 tasks.withType<Jar> {
-//    archiveFileName.set("rewheeldev.jar")
     manifest {
         attributes["Implementation-Version"] = archiveVersion
     }
-//    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
 }
 
-task("downloadFile") {
-    doLast {
-        download.run {
-            src("https://github.com/rewheeldev/common-utils/raw/main/common-utils-0.0.1.jar")
-            dest(buildDir)
-            overwrite(true)
-        }
-    }
-}
+//task("downloadFile") {
+//    doLast {
+//        download.run {
+//            src("https://github.com/rewheeldev/common-utils/raw/main/common-utils-0.0.1.jar")
+//            dest(buildDir)
+//            overwrite(true)
+//        }
+//    }
+//}
